948bcc3741887b3fa4107b3f7e3b2af01059ba7f
Quentin Cirri <155235483+QuentinCirri@users.noreply.github.com>
2024-04-10 09:16:38 +0200

    Quentin 10 avril 2024

---
+++ JS/main.js
@@ -0,0 +1,44 @@
+// for the header button
+var buttonHeader = document.getElementById("");
+
+// Add a click event listener
+buttonHeader.addEventListener("click", function () {
+  // Change the button's color
+  this.style.backgroundColor = "";
+  this.style.color = "";
+});
+
+// for the nav text
+var nav = document.getElementById("");
+
+// Add a click event listener
+nav.addEventListener("click", function () {
+  // Change the nav color
+  this.style.color = "";
+});
+
+// for buttons in the main
+var button = document.getElementById("");
+
+// Add a click event listener
+button.addEventListener("click", function () {
+  // Change the button's color
+  this.style.backgroundColor = "";
+  this.style.color = "";
+});
+
+// for the 3 logo in the footer
+var logo = document.getElementById("");
+// Add a click event listener
+logo.addEventListener("click", function () {
+  // Change the button's color
+  this.style.backgroundColor = "";
+});
+
+// for the FAQ text bar
+var bar = document.getElementById("");
+// Add a click event listener
+bar.addEventListener("click", function () {
+  // Change the button's color
+  this.style.backgroundColor = "";
+});
