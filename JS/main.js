ff30d9b849a1d4fb06f1f9cec7ef542f3848fd79
Quentin Cirri <155235483+QuentinCirri@users.noreply.github.com>
2024-04-15 09:57:21 +0200

    Menu quentin
    le menu scroll up

--- JS/main.js
+++ JS/main.js
@@ -1,44 +1,17 @@
-// for the header button
-var buttonHeader = document.getElementById("");
+let lastScrollTop = 0;
 
-// Add a click event listener
-buttonHeader.addEventListener("click", function () {
-  // Change the button's color
-  this.style.backgroundColor = "";
-  this.style.color = "";
-});
-
-// for the nav text
-var nav = document.getElementById("");
-
-// Add a click event listener
-nav.addEventListener("click", function () {
-  // Change the nav color
-  this.style.color = "";
-});
-
-// for buttons in the main
-var button = document.getElementById("");
-
-// Add a click event listener
-button.addEventListener("click", function () {
-  // Change the button's color
-  this.style.backgroundColor = "";
-  this.style.color = "";
-});
-
-// for the 3 logo in the footer
-var logo = document.getElementById("");
-// Add a click event listener
-logo.addEventListener("click", function () {
-  // Change the button's color
-  this.style.backgroundColor = "";
-});
-
-// for the FAQ text bar
-var bar = document.getElementById("");
-// Add a click event listener
-bar.addEventListener("click", function () {
-  // Change the button's color
-  this.style.backgroundColor = "";
-});
+document.addEventListener(
+  "scroll",
+  () => {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+      // Scrolling down
+      document.querySelector("header").style.top = "-150px";
+    } else {
+      // Scrolling up
+      document.querySelector("header").style.top = "0";
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  },
+  false
+);
